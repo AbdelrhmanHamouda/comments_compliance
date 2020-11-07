f697e8538c923372a431f266ceab4f2ebf832b86
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2020-11-07 13:17:30 +0100

    Add Help function

--- comments_compliance.sh
+++ comments_compliance.sh
@@ -12,7 +12,21 @@ SORT_OUTPUT='False'
 FILE_EXTENSION='.c'
 PATH='.'
 
-
+# Define the help function
+function Help(){
+   # Display Help
+   echo "The script searches for all C-files that contain less than a specific percentage of commented lines. "
+   echo
+   echo "Syntax: comments_compliance.sh [-p|x|i|s|h] [path]"
+   echo "parameters:"
+   echo "p     Set the percentage for comment-lines. The provided value should be from 0 to 100 (default is '30')."
+   echo "x     Set the file extension to be checked (default is '.c')."
+   echo "i     Inverts the operation. Meaning that the output will be for the files that match or exceed the defined percentage."
+   echo "s     Sort the output alphanumerically."
+   echo "h     Print this help."
+   echo "path  Set the path to be used (default is '\$PWD')."
+   echo
+}
 
 # Get parameters if provided
 while getopts p:x:ish parameters
